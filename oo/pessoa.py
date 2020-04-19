eb31394c824c5e252a012cdd4cbfd021816e1e44
huidemar <huidemar@gmail.com>
2020-04-19 18:30:31 -0300

    Criado atributo complexo da classe pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,20 +1,22 @@
 class Pessoa:
 
-    def __init__(self, nome=None, idade=100):
+    def __init__(self, *filhos, nome=None, idade=100):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return 'Olá {}'.format(id(self))
 
 
 if __name__ == '__main__':
-    p = Pessoa('Tício')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Huidemar'
-    print(p.nome)
-    print(p.idade)
+    ticio = Pessoa(nome='Tício')
+    mevio = Pessoa(ticio, nome='Mévio')
+    print(Pessoa.cumprimentar(mevio))
+    print(id(mevio))
+    print(mevio.cumprimentar())
+    print(mevio.nome)
+    print(mevio.idade)
+    for filho in mevio.filhos:
+        print(filho.nome)
 
