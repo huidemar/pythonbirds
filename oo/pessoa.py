de02a468e94bb4ca2c4704a8b450bf2622668a05
huidemar <huidemar@gmail.com>
2020-04-19 18:24:00 -0300

    Criado os atributos: nomes e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,20 @@
 class Pessoa:
+
+    def __init__(self, nome=None, idade=100):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return 'Olá {}'.format(id(self))
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Tício')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Huidemar'
+    print(p.nome)
+    print(p.idade)
+
